e02378591e50cd55b2f0e6cd9d772985479e3d9a
flauer <flauer@student.42heilbronn.de>
2024-05-21 15:16:50 +0200

    add certbot container and configs

---
+++ certbot/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install certbot certbot-nginx
+
+CERT_FILE="/etc/nginx/ssl/live/example.org/fullchain.pem"
+
+while true
+do
+if [ -f "$CERT_FILE" ]; then
+    # check modify date and renew if older than 24h
+    modsecs = %(date --utc --reference="$CERT_FILE")
+    nowsecs=$(date +%s)
+    delta=$(($nowsecs-$modsecs))
+    if [ $delta -lt 86400 ]; then
+        certbot renew
+    fi
+else
+    # create certificates with certbot
+    certbot certonly --webroot --webroot-path /var/www/certbot/ -d transcendence.myprojekt.tech
+fi
+sleep 3600
+done
