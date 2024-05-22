f86028ba11d3d02d82952b5150192a9699b37938
flolau2k <flauer@student.42heilbronn.de>
2024-05-22 07:12:03 +0000

    cleanup and move pip install to dockerfile

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -4,9 +4,7 @@ set -eux
 
 rm -f /healthy
 
-python3 -m venv .venv
 source .venv/bin/activate
-pip install certbot certbot-nginx
 
 CERT_FILE="/etc/letsencrypt/live/transcendence.myprojekt.tech/fullchain.pem"
 
@@ -14,8 +12,6 @@ while true
 do
 if [ -f "$CERT_FILE" ]; then
     # stop running validation container
-    docker stop nginx-validation
-    docker rm nginx-validation
     # check modify date and renew if older than 24h
     modsecs=$(date --utc --reference="$CERT_FILE" +%s)
     nowsecs=$(date +%s)
@@ -23,6 +19,8 @@ if [ -f "$CERT_FILE" ]; then
     if [ $delta -lt 86400 ]; then
         certbot renew
     fi
+    docker stop nginx-validation || true
+    docker rm nginx-validation || true
     touch /healthy
 else
     # create certificates with certbot
@@ -33,7 +31,8 @@ else
         --agree-tos \
         -m flauer@student.42heilbronn.de \
         -d transcendence.myprojekt.tech \
-        --cert-name transcendence.myprojekt.tech
+        --cert-name transcendence.myprojekt.tech \
+        --test-cert
     do
         sleep 2
     done
