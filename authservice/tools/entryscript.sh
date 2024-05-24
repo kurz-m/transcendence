c934db4a4ccc5bcd9bb9afb32b1f6761e7334b57
sanjok <bsanjok@live.com>
2024-05-24 14:27:52 +0000

    minor fixes to docker compose and Dockerfile - add bind volume to authapp - update entryscript.sh

--- authservice/tools/entryscript.sh
+++ authservice/tools/entryscript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+# cd /app
+pip install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn -b :8000 usermanager.wsgi
