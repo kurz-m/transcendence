215b4293ceaa579bdb01533f4d791e37b2650547
sanjok <bsanjok@live.com>
2024-06-01 21:07:31 +0000

    Added Friends Related endpoints: - Make Friend Requests - List Friend Requests - Accept Friend Requests - List Friends - Delete Friends - Load a friend's details

--- authservice/authapp/players/friendrequest.py
+++ authservice/authapp/players/friendrequest.py
@@ -2,10 +2,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from players.models import FriendRequest, Players, User
-from players.serializers import FriendRequestCreateSerializer, FriendRequestSerializer
+from players.serializers import FriendRequestCreateSerializer, FriendRequestSerializer, AcceptFriendRequestSerializer
 from rest_framework.views import APIView
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
+from rest_framework import permissions
 
 
 class FriendRequestSendView(APIView):
@@ -33,3 +34,42 @@ class FriendRequestSendView(APIView):
             return HttpResponse('Friend request is already sent.', status=status.HTTP_400_BAD_REQUEST)
         serializer.save(sender=sender)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AcceptFriendRequestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = AcceptFriendRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        request_id = serializer.validated_data['request_id']
+
+        try:
+            friend_request = FriendRequest.objects.get(id=request_id, receiver__user=request.user)
+        except FriendRequest.DoesNotExist:
+            return HttpResponse('Friend request not found or not authorized', status=status.HTTP_400_BAD_REQUEST)
+
+        sender = friend_request.sender
+        receiver = friend_request.receiver
+        sender.add_friend(receiver)
+        friend_request.delete()
+
+        return Response({'message': 'Friend request accepted'}, status=status.HTTP_200_OK)
+
+
+class FriendRequestsAPIView(APIView):
+    """
+    API endpoint that allows to list friend requests received by a player.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        try:
+            player = Players.objects.get(user=user)
+        except Players.DoesNotExist:
+            return Response({'error': 'Player not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        friend_requests = FriendRequest.objects.filter(receiver=player)
+        serializer = FriendRequestSerializer(friend_requests, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- authservice/authapp/usermanager/urls.py
+++ authservice/authapp/usermanager/urls.py
@@ -23,7 +23,7 @@ from django.urls import include, path
 from leaderboard.views import LeaderboardViewSet
 from remoteauth.views import callbackCode, authorizeCall, loggedIn, logOut
 from remoteauth.utils import ServeMedia
-from players.friendrequest import FriendRequestSendView
+from players.friendrequest import FriendRequestSendView, AcceptFriendRequestView, FriendRequestsAPIView
 
 router = routers.DefaultRouter()
 # router.register(r'leaderboard', LeaderboardViewSet)
@@ -39,7 +39,11 @@ urlpatterns = [
     path('api/auth/callback', callbackCode.as_view(), name='auth-callback'),
     path('api/auth/loggedin', loggedIn.as_view(), name='auth-loggedin'),
     path('api/media/<path:filename>', ServeMedia.as_view(), name='serve_media'),
-    path('api/friends/sendrequest', FriendRequestSendView.as_view(), name='send_friend_request')
+    path('api/friends/sendrequest', FriendRequestSendView.as_view(), name='send_friend_request'),
+    path('api/friends/acceptrequest', AcceptFriendRequestView.as_view(), name='accept_friend_request'),
+    path('api/friends/requests', FriendRequestsAPIView.as_view(), name='list_friend_requests'),
+    path('api/friends', views.FriendsApiView.as_view(), name='list_friends'),
+    path('api/friends/<str:username>', views.FriendsApiView.as_view(), name='delete_or_get_friend'),
 ]
 
 if not settings.DEBUG:
