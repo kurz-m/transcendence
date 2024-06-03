3ee31b26386dad5a961f42d39e1d7f9aa4523fbf
kurz-m <m.kurz92@gmail.com>
2024-06-03 16:02:35 +0000

    remove unnecessary html code

--- html/static/js/views/MatchHistoryView.js
+++ html/static/js/views/MatchHistoryView.js
@@ -56,45 +56,19 @@ export default class extends AbstractView {
     <div class="window">
         <div class="topbar">
             <button id="back-button" onclick="history.back()" class="icon-button">
-                <img class="icon" src="./static/media/back.svg" alt="Back" draggable="false" (dragstart)="false;">
+                <img class="icon" src="./static/media/back.svg" alt="Back" draggable="false" (dragstart)="false;" />
             </button>
             <div class="title">Match History</div>
             <a href="/" class="icon-button" data-link>
-                <img class="icon" src="./static/media/home.svg" alt="Main Menu" draggable="false" (dragstart)="false;">
+                <img class="icon" src="./static/media/home.svg" alt="Main Menu" draggable="false" (dragstart)="false;" />
             </a>
         </div>
         <div class="content">
             <div class="match-list">
-                <div class="scroll-matches">
-                    <div class="list-item">
-                        <div class="match-icon">S</div>
-                        <div class="match-date">28.08.28 08:58</div>
-                        <img class="trophy" src="./static/media/trophy-gold.svg" draggable="false" (dragstart)="false;">
-                            <div class="match-result">
-                                <div class="left-player">Aaron Rabenstein</div>
-                                <div class="score">
-                                    <div class="left-score">11</div>
-                                    <div class="score-colon">:</div>
-                                    <div class="right-score">8</div>
-                                </div>
-                                <div class="right-player">Markuz Kurz</div>
-                            </div>
-                    </div>
-                    <div class="list-item">
-                        <button class="match-button">T</button>
-                        <div class="match-date">21.04.24 16:04</div>
-                        <img class="trophy" src="./static/media/trophy-gold.svg" draggable="false" (dragstart)="false;">
-                        <div class="match-result">
-                        <div class="tournament-rank">1st</div>
-                        <div class="match-text">out of</div>
-                        <div class="tournament-players">5</div>
-                        <div class="match-text"> players</div>
-                        </div>
-                    </div>
-                </div>
+                <div class="scroll-matches"></div>
             </div>
         </div>
-        </div>
+    </div>
         `
     }
 
@@ -131,7 +105,7 @@ export default class extends AbstractView {
             <div class="match-date">${date}</div>
             <img class="trophy" src="${image}" draggable="false" (dragstart)="false;">
             <div class="match-result">
-                <div class="tournament-rank">${rank + '.'}</div>
+                <div class="tournament-rank">${rank + '. '}</div>
                 <div class="match-text">out of</div>
                 <div class="tournament-players">${number_of_players}</div>
                 <div class="match-text"> players</div>
