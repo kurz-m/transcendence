82c5fa098eeb05bdd0f2841d047b7e529199a5c1
sanjok <bsanjok@live.com>
2024-06-03 14:40:49 +0000

    removed unwanted comment

--- gameplayservice/gameplay/game/permission.py
+++ gameplayservice/gameplay/game/permission.py
@@ -6,10 +6,8 @@ class IsOwnerAndNotDelete(permissions.BasePermission):
     but never allow deletion.
     """
     def has_object_permission(self, request, view, obj):
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
-        
         if request.method == 'DELETE':
             return False
 
         return obj.user == request.user
+
