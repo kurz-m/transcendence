01704b851621902ab1633e3b1bb281c43fdc20bd
sanjok <bsanjok@live.com>
2024-06-03 14:38:18 +0000

    fixed player details update method. user can now update everything except username.

--- authservice/authapp/players/serializers.py
+++ authservice/authapp/players/serializers.py
@@ -7,12 +7,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+        read_only_fields = ['username']
 
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+    def update(self, instance, validated_data):
+        validated_data.pop('username', None)
+        return super().update(instance, validated_data)
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,12 +20,31 @@ class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Players
         fields = ['id', 'user', 'profile_img_uri', 'two_factor', 'online_status', 'friends']
+        extra_kwargs = {
+            'url': {'view_name': 'players-detail'},
+            'user': {'view_name': 'user-detail'}
+        }
     
     def create(self, validated_data):
         user_data = validated_data.pop('user')
         user = User.objects.create(**user_data)
         player = Players.objects.create(user=user, **validated_data)
         return player
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', None)
+        if user_data:
+            user_instance = instance.user
+            user_serializer = self.fields['user']
+            user_data.pop('username', None)
+            user_instance = user_serializer.update(user_instance, user_data)
+
+        instance.profile_img_uri = validated_data.get('profile_img_uri', instance.profile_img_uri)
+        instance.two_factor = validated_data.get('two_factor', instance.two_factor)
+        instance.online_status = validated_data.get('online_status', instance.online_status)
+        instance.friends.set(validated_data.get('friends', instance.friends.all()))
+        instance.save()
+        return instance
 
 
 class FriendRequestSerializer(serializers.HyperlinkedModelSerializer):
