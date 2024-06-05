e4bccbf537da65674ba96610db1c8a79507221f9
sanjok <bsanjok@live.com>
2024-06-05 10:44:19 +0000

    format created_date and remove game_date from GameSerializer. created_date from score endpoint is now default game date and time.

--- gameplayservice/gameplay/game/serializers.py
+++ gameplayservice/gameplay/game/serializers.py
@@ -5,7 +5,7 @@ from game.models import Game, Score
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ['id', 'game_date', 'game_type']
+        fields = ['id', 'game_type']
 
         read_only_fields = ['user',]
 
@@ -19,6 +19,15 @@ class GameSerializer(serializers.ModelSerializer):
 class ScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Score
-        fields = ['opponent', 'own_score', 'opponent_score', 'win', 'game_type', 'game_id', 'rank', 'number_of_players']
+        fields = ['opponent', 'own_score', 'opponent_score', 'win', 'game_type', 'game_id', 'rank', 'number_of_players', 'created_date']
 
         read_only_fields = ['user', 'created_date']
+
+    def to_representation(self, instance):
+        """Customize the output to exclude microseconds in the datetime string."""
+        ret = super().to_representation(instance)
+        created_date = instance.created_date
+        if created_date:
+            created_date = created_date.replace(microsecond=0)
+            ret['created_date'] = created_date.strftime("%Y-%m-%d %H:%M:%S")
+        return ret
