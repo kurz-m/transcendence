450b68ac7b6cc1a7e25bc7129d4b733656e59580
sanjok <bsanjok@live.com>
2024-06-07 14:55:43 +0000

    removed unused leaderboard app and model

--- authservice/authapp/leaderboard/serializers.py
+++
@@ -1,8 +0,0 @@
-from leaderboard.models import Leaderboard
-from rest_framework import serializers
-
-
-class LeaderboardSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Leaderboard
-        fields = ['game_name', 'score', 'game_played_date']
