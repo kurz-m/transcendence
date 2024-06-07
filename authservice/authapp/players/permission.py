64aa89c660901baeb524420e8bad6ec4e702b05d
sanjok <bsanjok@live.com>
2024-06-07 12:09:05 +0000

    minor fixes and permission adjustments

--- authservice/authapp/players/permission.py
+++ authservice/authapp/players/permission.py
@@ -13,9 +13,6 @@ class IsOwnerAndNotDelete(permissions.BasePermission):
         if request.method in ('GET'):
             if obj.user == request.user:
                 return True
-
-            player = Players.objects.get(user=request.user)
-            return player.friends.filter(id=obj.id).exists()
         
         if request.method in ('PUT'):
             if obj.user == request.user:
@@ -36,9 +33,6 @@ class IsOwnerAndNotDeleteUsers(permissions.BasePermission):
         if request.method in ('GET'):
             if obj == request.user:
                 return True
-
-            player = Players.objects.get(user=request.user)
-            return player.friends.filter(id=obj.id).exists()
         
         if request.method in ('PUT'):
             if obj == request.user:
@@ -46,12 +40,33 @@ class IsOwnerAndNotDeleteUsers(permissions.BasePermission):
     
         return False
 
+class IsOwnerAndFriends(permissions.BasePermission):
+    """
+    Custom permission to allow only owners of an object to get and delete.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            if obj == request.user:
+                return True
 
-class IsOwnerAndNotDeleteFriends(permissions.BasePermission):
+        if request.method in ('GET'):
+            if obj == request.user:
+                return True
+
+            player = Players.objects.get(user=request.user)
+            return player.friends.filter(id=obj.id).exists()
+    
+        return False
+
+
+class IsOwnerAndNotDeleteFriendsRequests(permissions.BasePermission):
     """
     Custom permission to allow only owners of an object to get, edit,
     but never allow deletion.
     """
     def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            return False
+
         return obj.sender == request.user
 
