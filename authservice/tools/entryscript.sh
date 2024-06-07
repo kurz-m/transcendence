d66496bf310dd8cad1908413ad8a118a33846fdd
sanjok <bsanjok@live.com>
2024-06-07 14:56:02 +0000

    fix player model migration issue

--- authservice/tools/entryscript.sh
+++ authservice/tools/entryscript.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # cd /app
 pip install -r requirements.txt
+python manage.py makemigrations players
 python manage.py makemigrations && python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn -b :8000 usermanager.wsgi
