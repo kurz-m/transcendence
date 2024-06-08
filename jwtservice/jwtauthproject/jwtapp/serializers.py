02cf4278c06313ac26ebd1e74e15c65e2621fde9
sanjok <bsanjok@live.com>
2024-06-08 15:36:49 +0000

    removing players model, viewset and serializer as there is no use of it in jwtservice

--- jwtservice/jwtauthproject/jwtapp/serializers.py
+++ jwtservice/jwtauthproject/jwtapp/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from jwtapp.models import Players
+# from jwtapp.models import Players
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -8,15 +8,15 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
 
-class PlayerSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer()
+# class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+#     user = UserSerializer()
 
-    class Meta:
-        model = Players
-        fields = ['id', 'user', 'profile_img_url', 'two_factor', 'online_status', 'friends']
+#     class Meta:
+#         model = Players
+#         fields = ['id', 'user', 'profile_img_url', 'two_factor', 'online_status', 'friends']
     
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = User.objects.create(**user_data)
-        player = Players.objects.create(user=user, **validated_data)
-        return player
+#     def create(self, validated_data):
+#         user_data = validated_data.pop('user')
+#         user = User.objects.create(**user_data)
+#         player = Players.objects.create(user=user, **validated_data)
+#         return player
