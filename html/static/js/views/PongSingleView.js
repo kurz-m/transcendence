34e4d6e3362d6e70da2e2a060437d2ce6fcab074
kurz-m <m.kurz92@gmail.com>
2024-06-08 08:03:06 +0000

    syntax update and change of input class

--- html/static/js/views/PongSingleView.js
+++ html/static/js/views/PongSingleView.js
@@ -11,21 +11,21 @@ export default class extends AbstractView {
         return `
         <div class="window">
             <div class="topbar">
-            <button id="back-button" onclick="history.back()" class="icon-button">
-                <img class="icon" src="./static/media/back.svg" alt="Back" draggable="false" (dragstart)="false;">
-            </button>
-            <div class="title">Single Game</div>
-            <a href="/" class="icon-button" data-link>
-                <img class="icon" src="./static/media/home.svg" alt="Main Menu" draggable="false" (dragstart)="false;">
-            </a>
+                <button id="back-button" onclick="history.back()" class="icon-button">
+                    <img class="icon" src="./static/media/back.svg" alt="Back" draggable="false" (dragstart)="false;" />
+                </button>
+                <div class="title">Single Game</div>
+                <a href="/" class="icon-button" data-link>
+                    <img class="icon" src="./static/media/home.svg" alt="Main Menu" draggable="false" (dragstart)="false;" />
+                </a>
             </div>
             <div class="content">
-            <a id="ai-button" href="/pong-game" class="a-large-button" data-link>Play vs. AI</a>
-            <div class="label-field-button">
-                <div class="label">Play vs.</div>
-                <input id="opponent-name" class="text-field" type="text" placeholder="Guest">
-                <a id="play-button" href="/pong-game" class="a-small-button" data-link>play</a>
-            </div>
+                <a id="ai-button" href="/pong-game" class="a-large-button" data-link>Play vs. AI</a>
+                <div class="input-segment">
+                    <div class="label">Play vs.</div>
+                    <input id="opponent-name" class="text-field" type="text" placeholder="Guest" />
+                    <a id="play-button" href="/pong-game" class="a-small-button" data-link>play</a>
+                </div>
             </div>
         </div>
         `
