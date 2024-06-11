cd717fbf82796acb4b666b3ac43d3ee45595aba2
sanjok <bsanjok@live.com>
2024-06-11 11:46:03 +0000

    encrypted oauth token appearing in url during mfa login

--- authservice/authapp/remoteauth/utils.py
+++ authservice/authapp/remoteauth/utils.py
@@ -1,6 +1,8 @@
 import os
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from cryptography.fernet import Fernet
+import base64
 
 class ServeMedia(APIView):
     permission_classes = [IsAuthenticated]
@@ -13,8 +15,6 @@ class ServeMedia(APIView):
         else:
             raise Http404
 
-TOKEN_URL = "https://api.intra.42.fr/oauth/token"
-
 
 def authorize():
     client_id = os.getenv("CLIENT_ID")
@@ -25,11 +25,15 @@ def authorize():
     return authorize_url
 
 
-# def serve_media(request, filename):
+def encrypt_token(token, key):
+    fernet = Fernet(key)
+    encrypted_token = fernet.encrypt(token.encode())
+    return encrypted_token.decode()
 
-#     file_path = os.path.join(settings.MEDIA_ROOT, filename)
-#     if os.path.exists(file_path):
-#         with open(file_path, 'rb') as f:
-#             return HttpResponse(f.read(), content_type='image/jpeg')
-#     else:
-#         raise Http404
+def decrypt_token(encrypted_token, key):
+    fernet = Fernet(key)
+    decrypted_token = fernet.decrypt(encrypted_token.encode())
+    return decrypted_token.decode()
+
+def generate_key():
+    return base64.urlsafe_b64encode(os.urandom(32))
--- authservice/authapp/remoteauth/views.py
+++ authservice/authapp/remoteauth/views.py
@@ -18,10 +18,12 @@ from django.http import JsonResponse
 from remoteauth.authentication import RemoteJWTAUthentication
 from rest_framework.permissions import AllowAny
 from remoteauth.middleware.logstashmiddleware import LogstashMiddleware
+from remoteauth.utils import encrypt_token, decrypt_token, generate_key
 
-def get_user_info(access_token):
+
+def get_user_info(oauth_token):
     url = 'https://api.intra.42.fr/v2/me'
-    header = {'Authorization': f'Bearer {access_token}'}
+    header = {'Authorization': f'Bearer {oauth_token}'}
 
     response = requests.get(url, headers=header)
 
@@ -32,6 +34,24 @@ def get_user_info(access_token):
     else:
         print(f"Request failed with status code {response.status_code}")
         return None
+
+
+def success_login(player, oauth_token):
+    jwt_service_url = 'http://jwtservice:8000/api-jwt/token/generate'
+    send_data = {'user_id': player.user.id, 'username': player.user.username, 'email': player.user.email, 'oauth_token': oauth_token}
+    response = requests.post(jwt_service_url, json=send_data, headers={'Content-Type': 'application/json'})
+    response.raise_for_status()
+    data = response.json()
+    refresh_str = data.get('refresh_jwt_token')
+    refresh = RefreshToken(refresh_str)
+
+    return_data = {'profile_image_url': player.profile_img_url, 'detail': 'Successful operation. Cookies set with JWT token, username, and user ID'}
+    return_json = json.dumps(return_data)
+    oauth_response = HttpResponse(return_json, content_type='application/json')
+    oauth_response.set_cookie('access_token', refresh.access_token, httponly=True, secure=True, samesite='Lax', max_age=86400)
+    oauth_response.set_cookie('user', player.user, httponly=False, secure=True, samesite='Lax', max_age=86400)
+    oauth_response.set_cookie('player_id', player.id, httponly=False, secure=True, samesite='Lax', max_age=86400)
+    return oauth_response
 
 
 def create_player_from_user_info(user_info):
@@ -62,63 +82,52 @@ class authorizeCall(APIView):
         return_data = {'location': authorize()}
         login_response = JsonResponse(return_data)
         if token:
-            login_response.delete_cookie('access_token', path='/', domain=None)
-            login_response.delete_cookie('user', path='/', domain=None)
-            login_response.delete_cookie('2fa', path='/', domain=None)
-            login_response.delete_cookie('player_id', path='/', domain=None)
+            login_response.delete_cookie('access_token', path='/', domain=None, samesite='Lax')
+            login_response.delete_cookie('user', path='/', domain=None, samesite='Lax')
+            login_response.delete_cookie('player_id', path='/', domain=None, samesite='Lax')
             login_response.status_code = 200
         return login_response
+
 
 class loggedIn(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request, format=None):
         return Response({'detail': 'Successful operation. user is logged in.'})
 
+
 class logOut(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def post(self, request, format=None):
         middleware_instance = LogstashMiddleware(get_response=None)
         oauth_response = HttpResponse("Successful operation. User logged out and cookies cleared.")
-        oauth_response.delete_cookie('access_token', path='/', domain=None)
-        oauth_response.delete_cookie('user', path='/', domain=None)
-        oauth_response.delete_cookie('2fa', path='/', domain=None)
-        oauth_response.delete_cookie('player_id', path='/', domain=None)
+        oauth_response.delete_cookie('access_token', path='/', domain=None, samesite='Lax')
+        oauth_response.delete_cookie('user', path='/', domain=None, samesite='Lax')
+        oauth_response.delete_cookie('player_id', path='/', domain=None, samesite='Lax')
         oauth_response.status_code = 200
         middleware_instance.log_info(request, "user logged out")
         return oauth_response
+
 
 class mfaLogin(APIView):
     def post(self, request, format=None):
         middleware_instance = LogstashMiddleware(get_response=None)
         mfa_token = request.data.get('token', None)
         user_id = request.data.get('user_id', None)
-        oauth_token = request.data.get('oauth_token', None)
-
+        encrypted_oauth_token = request.data.get('oauth_token', None)
+        try:
+            oauth_token = decrypt_token(encrypted_oauth_token, settings.ENCRYPTION_KEY)
+        except:
+            return Response("Token decryption failed", status=status.HTTP_400_BAD_REQUEST)
         mfa_verify_url = 'http://twofactorservice:8000/api-mfa/verify'
         data = {'token': mfa_token, 'user_id': user_id}
         response = requests.post(mfa_verify_url, json=data, headers={'Content-Type': 'application/json'})
         if response.status_code is status.HTTP_200_OK:
             player = Players.objects.get(user__id=user_id)
-            jwt_service_url = 'http://jwtservice:8000/api-jwt/token/generate'
-            send_data = {'user_id': player.user.id, 'username': player.user.username, 'email': player.user.email, 'access_token': oauth_token}
-            response = requests.post(jwt_service_url, json=send_data, headers={'Content-Type': 'application/json'})
-            response.raise_for_status()
-            data = response.json()
-            refresh_str = data.get('refresh_jwt_token')
-            refresh = RefreshToken(refresh_str)
-
-            return_data = {'profile_image_url': player.profile_img_url, 'detail': 'Successful operation. Cookies set with JWT token, username, and user ID'}
-            return_json = json.dumps(return_data)
-            oauth_response = HttpResponse(return_json, content_type='application/json')
-            oauth_response.set_cookie('access_token', refresh.access_token, httponly=True, secure=True, samesite='Lax')
-            oauth_response.set_cookie('user', player.user, httponly=False, secure=True, samesite='Lax')
-            oauth_response.set_cookie('player_id', player.id, httponly=False, secure=True, samesite='Lax')
             middleware_instance.log_info(request, "Valid MFA Token!")
-            return oauth_response
+            return success_login(player, oauth_token)
         else:
             middleware_instance.log_info(request, "Invalid MFA Token!")
             return Response({'Invalid mfa token.'}, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class callbackCode(APIView):
@@ -139,32 +148,16 @@ class callbackCode(APIView):
             }
             response = requests.post(token_url, data=data)
             if response.status_code == 200:
-                access_token = response.json().get('access_token')
-                if not access_token:
+                oauth_token = response.json().get('access_token')
+                if not oauth_token:
                     return HttpResponseBadRequest('Missing Access Token in response received from 42 oauth.')
-                player = get_user_info(access_token=access_token)
-                if player.two_factor is True:
-                    return Response({'two_factor': "true", 'user_id': player.user.id, 'oauth_token': access_token}, status=status.HTTP_200_OK)
-                jwt_service_url = 'http://jwtservice:8000/api-jwt/token/generate'
-                send_data = {'user_id': player.user.id, 'username': player.user.username, 'email': player.user.email, 'access_token': access_token}
-                send_json = json.dumps(send_data)
-                response = requests.post(jwt_service_url, json=send_data, headers={'Content-Type': 'application/json'})
-                response.raise_for_status()
-                data = response.json()
-                refresh_str = data.get('refresh_jwt_token')
-                refresh = RefreshToken(refresh_str)
-                if player and refresh and player.two_factor is False:
-                    return_data = {'profile_image_url': player.profile_img_url, 'detail': 'Successful operation. Cookies set with JWT token, username, and user ID'}
-                    return_json = json.dumps(return_data)
-                    oauth_response = HttpResponse(return_json, content_type='application/json')
-                    oauth_response.set_cookie('access_token', refresh.access_token, httponly=True, secure=True, samesite='Lax')
-                    oauth_response.set_cookie('user', player.user, httponly=False, secure=True, samesite='Lax')
-                    oauth_response.set_cookie('player_id', player.id, httponly=False, secure=True, samesite='Lax')
-                    middleware_instance.log_info(request, "User logged in successfully.")
-                    return oauth_response
-                else:
+                player = get_user_info(oauth_token=oauth_token)
+                if player and player.two_factor is True:
                     middleware_instance.log_info(request, "Two factor is active for user.")
-                    return Response({'two_factor': "true", 'user_id': player.user.id, 'oauth_token': access_token}, status=status.HTTP_100_CONTINUE)
+                    oauth_token = encrypt_token(oauth_token, settings.ENCRYPTION_KEY)
+                    return Response({'two_factor': "true", 'user_id': player.user.id, 'oauth_token': oauth_token}, status=status.HTTP_200_OK)
+                middleware_instance.log_info(request, "User logged in successfully.")
+                return success_login(player, oauth_token)
             else:
                 return HttpResponseBadRequest('Invalid Authorization Request to 42 oauth.')
         else:
