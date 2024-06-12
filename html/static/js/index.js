941577035d9ffe6af6632de5a86769967b2706fc
kurz-m <m.kurz92@gmail.com>
2024-06-12 22:55:46 +0200

    minor updates for offline support - add toastbox - inline svg for offline support

--- html/static/js/index.js
+++ html/static/js/index.js
@@ -20,10 +20,39 @@ let reloadIconTimeout;
 setInterval(() => {
     const newStatus = navigator.onLine;
     if (newStatus !== isOnline) {
+        let toastBox = document.getElementById('toastBox');
         isOnline = newStatus;
         if (!isOnline) {
             /* TODO: show popup when offline */
+            let toast = document.createElement('div');
+            toast.classList.add('toast');
+            toast.classList.add('toast-warning');
+            toast.classList.add('error');
+            toast.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                </svg>
+                Oops! Looks like you are offline.<br />Reconnect for full functionality.
+            `;
+            toastBox.appendChild(toast);
+            setTimeout(() => {
+                toast.remove();
+            }, 6000);
         } else {
+            let toast = document.createElement('div');
+            toast.classList.add('toast');
+            toast.classList.add('toast-success');
+            toast.classList.add('success');
+            toast.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                </svg>
+                Back online! Don't Panic.
+            `;
+            toastBox.appendChild(toast);
+            setTimeout(() => {
+                toast.remove();
+            }, 6000);
             /* TODO: show popup for being online again */     
             clearTimeout(reloadIconTimeout);
             reloadIconTimeout = setTimeout(() => {
