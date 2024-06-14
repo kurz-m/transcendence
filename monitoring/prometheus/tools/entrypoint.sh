7dcd4522900e12bc212176e2900a19c24883e76e
sanjok <bsanjok@live.com>
2024-06-15 01:44:29 +0200

    implemented alert manager and port adjustment for services in docker compose file.

--- monitoring/prometheus/tools/entrypoint.sh
+++ monitoring/prometheus/tools/entrypoint.sh
@@ -12,4 +12,24 @@ echo "$web_yml_content" > /etc/prometheus/web.yml
 
 DATASOURCE_CONFIG=/etc/prometheus/data/datasource.yml
 
+rm /prometheus/alertmanager.yml
+
+generate_alertmanager_config=$(cat <<EOF
+route:
+  group_by: ['alertname']
+  receiver: 'email-notifications'
+
+receivers:
+  - name: 'email-notifications'
+    email_configs:
+      - to: 'sanjok.bhatta@gmail.com'
+        from: 'alerts@myprojekt.tech'
+        smarthost: '${MAILJET_HOST}'
+        auth_username: '${MAILJET_API_KEY}'
+        auth_password: '${MAILJET_SECRET}'
+        auth_identity: ''
+EOF
+)
+echo "$generate_alertmanager_config" > /prometheus/alertmanager.yml
+
 prometheus --config.file=/etc/prometheus/prometheus.yml --web.config.file=/etc/prometheus/web.yml
